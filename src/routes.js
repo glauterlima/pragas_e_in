e232793282119d4418dae970d0b28bd40fbcf5d2
Glauter Santos <glauterlima@gmail.com>
2019-12-16 11:07:07 -0300

    Criando loader de models - concluido

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Glauter Santos',
+    email: 'glauterlima@gmail.com',
+    password_hash: '123456',
+  });
 
+  return res.json(user);
+});
 export default routes;
